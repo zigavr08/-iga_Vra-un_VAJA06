4fc9f2e4c7a7cb534fbca00749484d60098cda37
zigavr08 <vracun.ziga@gmail.com>
2025-11-07 09:59:30 +0100

    sprememba

--- Vaja6/obj/Debug/net8.0/Vaja6.AssemblyInfo.cs
+++ Vaja6/obj/Debug/net8.0/Vaja6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vaja6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b805557c01e9ebc754d139af985cdbc9610790ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea4e78084ce63ee6f3c35c2ca87253714576e605")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vaja6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vaja6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
